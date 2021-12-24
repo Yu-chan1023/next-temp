196b590fa3152c2ec9a63d699634fe27fa598e47
senkyo-minami <y.minami@senkyo.co>
2021-12-24 17:16:18 +0900

    error2

--- src/libs/client.js
+++ src/libs/client.js
@@ -1,6 +1,6 @@
 import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
-  serviceDomain: 'temp',
-  apiKey: e1d92235c007441291dc2a927fd92448ea58,
+  serviceDomain: SERVICE,
+  apiKey: API_KEY,
 });
